591ae9fa9d8246832190498a7b502401bd3b5808
akshat92384 <akshbhatia979@gmail.com>
2023-05-27 12:28:26 -0700

    win methods

--- connect4/src/Default/Frame.java
+++ connect4/src/Default/Frame.java
@@ -8,15 +8,39 @@ import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 
 public class Frame extends JPanel implements ActionListener, KeyListener  {
-	private String[][] board;
+	private static String[][] board;
+	private static int turn;
+	private static boolean redWon;
+	private static boolean yellowWon;
 	
 	public static void main(String[] args) {
 		Frame f = new Frame();
-		System.out.println(f);
+		while (redWon == false && yellowWon == false) {
+			System.out.println("Turn " + turn + "!");
+			
+			System.out.println(f);
+			if (turn % 2 != 0) {
+				
+				didYellowWin();
+				
+				turn++;
+			}
+			else {
+				
+				didRedWin();
+				turn++;
+			}
+		}
+		
+	
 		
 	}
 	public Frame() {
+		
 		board = new String [8][9];
+		turn = 1;
+		redWon = false;
+		yellowWon = false;
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[0].length; j++) {
 				board[i][j] = ".";
@@ -46,16 +70,83 @@ public class Frame extends JPanel implements ActionListener, KeyListener  {
 		}
 		return result;
 	}
+	public String[][] getBoard(){
+		return board;
+	}
+	public static boolean getRedWon() {
+		return redWon;
+	}
+	public static boolean getYellowWon() {
+		return yellowWon;
+	}
+	public static void didRedWin() {
+		for (int i = 0; i < board.length-5; i++) {
+			for (int j = 0; j < board[0].length-5; j++) {
+				if (board[i][j].equals("r")) {
+					if (board[i+1][j].equals("r")&&board[i+2][j].equals("r")&&board[i+3][j].equals("r")) {
+						redWon = true;
+					}
+					if (board[i][j+1].equals("r")&&board[i][j+2].equals("r")&&board[i][j+3].equals("r")) {
+						redWon = true;
+					}
+					if (board[i+1][j+1].equals("r")&&board[i+2][j+2].equals("r")&&board[i+3][j+3].equals("r")) {
+						redWon = true;
+					}
+				}
+			}
+		}
+	}
+	public static void didYellowWin() {
+		for (int i = 0; i < board.length-5; i++) {
+			for (int j = 0; j < board[0].length-5; j++) {
+				if (board[i][j].equals("y")) {
+					if (board[i+1][j].equals("y")&&board[i+2][j].equals("y")&&board[i+3][j].equals("y")) {
+						yellowWon = true;
+					}
+					if (board[i][j+1].equals("y")&&board[i][j+2].equals("y")&&board[i][j+3].equals("y")) {
+						yellowWon = true;
+					}
+					if (board[i+1][j+1].equals("y")&&board[i+2][j+2].equals("y")&&board[i+3][j+3].equals("y")) {
+						yellowWon = true;
+					}
+				}
+			}
+		}
+	}
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
+		
+		
+				
 		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		if (e.getKeyCode() == 97) {
+		System.out.println("1");	
+		}
+		if (e.getKeyCode() == 2) {
+			System.out.println("1");	
+		}
+		if (e.getKeyCode() == 3) {
+			System.out.println("1");
+		}
+		if (e.getKeyCode() == 4) {
+			System.out.println("1");
+		}
+		if (e.getKeyCode() == 5) {
+			System.out.println("1");
+		}
+		if (e.getKeyCode() == 6) {
+			System.out.println("1");
+		}
+		if (e.getKeyCode() == 7) {
+			System.out.println("1");
+		}
+
 	}
 
 	@Override
