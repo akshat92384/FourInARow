aeedc3a9ba3051739669480f8038d57e334f057a
akshat92384 <akshbhatia979@gmail.com>
2023-05-27 13:53:57 -0700

    s

--- connect4/src/Default/Frame.java
+++ connect4/src/Default/Frame.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JPanel;
 
@@ -12,22 +14,150 @@ public class Frame extends JPanel implements ActionListener, KeyListener  {
 	private static int turn;
 	private static boolean redWon;
 	private static boolean yellowWon;
+	private List<String> redCoinCol;
+    private List<String> redCoinRow;
+    private List<String> yellowCoinCol;
+    private List<String> yellowCoinRow;
 	
 	public static void main(String[] args) {
 		Frame f = new Frame();
-		while (redWon == false && yellowWon == false) {
+		while (redWon == false && yellowWon == false && turn <= 42) {
 			System.out.println("Turn " + turn + "!");
-			
+			System.out.println(redWon+" "+yellowWon);
 			System.out.println(f);
 			if (turn % 2 != 0) {
-				
-				didYellowWin();
-				
+				Scanner scanner = new Scanner(System.in);
+		        System.out.print("Enter your input: ");
+		        String userInput = scanner.nextLine();
+		        if (userInput.equals("1")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][1].equals(".")) {
+							board[i][1] = "Y";
+							break;
+						}
+					}
+		        }
+		        if (userInput.equals("2")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][2].equals(".")) {
+							board[i][2] = "Y";
+							break;
+						}
+					}
+		        }
+		        if (userInput.equals("3")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][3].equals(".")) {
+							board[i][3] = "Y";
+							break;
+						}
+					}
+		        }
+		        if (userInput.equals("4")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][4].equals(".")) {
+							board[i][4] = "Y";
+							break;
+						}
+					}
+		        }
+		        if (userInput.equals("5")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][5].equals(".")) {
+							board[i][5] = "Y";
+							break;
+						}
+					}
+		        }
+		        if (userInput.equals("6")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][6].equals(".")) {
+							board[i][6] = "Y";
+							break;
+						}
+					}
+		        }
+		        if (userInput.equals("7")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][7].equals(".")) {
+							board[i][7] = "Y";
+							break;
+						}
+					}
+		        }
+		        didYellowWin();
+		        if (yellowWon) {
+		        	System.out.println("Yellow wins!");
+		        	break;
+		        }
 				turn++;
 			}
 			else {
-				
-				didRedWin();
+				Scanner scanner = new Scanner(System.in);
+		        System.out.print("Enter your input: ");
+		        String userInput = scanner.nextLine();
+		        if (userInput.equals("1")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][1].equals(".")) {
+							board[i][1] = "R";
+							break;
+						}
+					}
+		        }
+		        if (userInput.equals("2")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][2].equals(".")) {
+							board[i][2] = "R";
+							break;
+						}
+					}
+		        }
+		        if (userInput.equals("3")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][3].equals(".")) {
+							board[i][3] = "R";
+							break;
+						}
+					}
+		        }
+		        if (userInput.equals("4")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][4].equals(".")) {
+							board[i][4] = "R";
+							break;
+						}
+					}
+		        }
+		        if (userInput.equals("5")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][5].equals(".")) {
+							board[i][5] = "R";
+							break;
+						}
+					}
+		        }
+		        if (userInput.equals("6")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][6].equals(".")) {
+							board[i][6] = "R";
+							break;
+						}
+					}
+		        }
+		        if (userInput.equals("7")) {
+		        	for (int i = board.length-1; i > 0; i--) {
+						if (board[i][7].equals(".")) {
+							board[i][7] = "R";
+							break;
+						}
+					}
+		        }
+		        
+		        didRedWin();
+		        if (redWon) {
+		        	System.out.println("Red wins!");
+		        	break;
+		        }
 				turn++;
 			}
 		}
@@ -82,14 +212,14 @@ public class Frame extends JPanel implements ActionListener, KeyListener  {
 	public static void didRedWin() {
 		for (int i = 0; i < board.length-5; i++) {
 			for (int j = 0; j < board[0].length-5; j++) {
-				if (board[i][j].equals("r")) {
-					if (board[i+1][j].equals("r")&&board[i+2][j].equals("r")&&board[i+3][j].equals("r")) {
+				if (board[i][j].equals("R")) {
+					if (board[i+1][j].equals("R")&&board[i+2][j].equals("R")&&board[i+3][j].equals("R")) {
 						redWon = true;
 					}
-					if (board[i][j+1].equals("r")&&board[i][j+2].equals("r")&&board[i][j+3].equals("r")) {
+					if (board[i][j+1].equals("R")&&board[i][j+2].equals("R")&&board[i][j+3].equals("R")) {
 						redWon = true;
 					}
-					if (board[i+1][j+1].equals("r")&&board[i+2][j+2].equals("r")&&board[i+3][j+3].equals("r")) {
+					if (board[i+1][j+1].equals("R")&&board[i+2][j+2].equals("R")&&board[i+3][j+3].equals("R1")) {
 						redWon = true;
 					}
 				}
@@ -99,14 +229,14 @@ public class Frame extends JPanel implements ActionListener, KeyListener  {
 	public static void didYellowWin() {
 		for (int i = 0; i < board.length-5; i++) {
 			for (int j = 0; j < board[0].length-5; j++) {
-				if (board[i][j].equals("y")) {
-					if (board[i+1][j].equals("y")&&board[i+2][j].equals("y")&&board[i+3][j].equals("y")) {
+				if (board[i][j].equals("Y")) {
+					if (board[i+1][j].equals("Y")&&board[i+2][j].equals("Y")&&board[i+3][j].equals("Y")) {
 						yellowWon = true;
 					}
-					if (board[i][j+1].equals("y")&&board[i][j+2].equals("y")&&board[i][j+3].equals("y")) {
+					if (board[i][j+1].equals("Y")&&board[i][j+2].equals("Y")&&board[i][j+3].equals("Y")) {
 						yellowWon = true;
 					}
-					if (board[i+1][j+1].equals("y")&&board[i+2][j+2].equals("y")&&board[i+3][j+3].equals("y")) {
+					if (board[i+1][j+1].equals("Y")&&board[i+2][j+2].equals("Y")&&board[i+3][j+3].equals("Y")) {
 						yellowWon = true;
 					}
 				}
@@ -121,32 +251,155 @@ public class Frame extends JPanel implements ActionListener, KeyListener  {
 				
 		
 	}
-
+	public void yellowTurn() {
+		
+	}
+	public void redTurn() {
+		
+	}
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		if (e.getKeyCode() == 97) {
-		System.out.println("1");	
+			for (int i = board.length; i > 0; i--) {
+				if (board[i][1].equals(".")) {
+					
+					break;
+				}
+			}
 		}
-		if (e.getKeyCode() == 2) {
-			System.out.println("1");	
-		}
-		if (e.getKeyCode() == 3) {
-			System.out.println("1");
-		}
-		if (e.getKeyCode() == 4) {
-			System.out.println("1");
-		}
-		if (e.getKeyCode() == 5) {
-			System.out.println("1");
-		}
-		if (e.getKeyCode() == 6) {
-			System.out.println("1");
-		}
-		if (e.getKeyCode() == 7) {
-			System.out.println("1");
-		}
-
+		
+//		int keyCode = e.getKeyCode();
+//
+//        
+//		if (keyCode == KeyEvent.VK_1) {
+//            boolean exists = false;
+//            for (String col : redCoinCol) {
+//                if (col.equals("1")) {
+//                    exists = true;
+//                    break;
+//                }
+//            }
+//
+//            if (!exists) {
+//                redCoinCol.add("1");
+//                if (turn % 2 != 0) {
+//                    board[7][1] = "Y";
+//                } else {
+//                    board[7][1] = "R";
+//                }
+//            }
+//        } else if (keyCode == KeyEvent.VK_2) {
+//            boolean exists = false;
+//            for (String col : redCoinCol) {
+//                if (col.equals("2")) {
+//                    exists = true;
+//                    break;
+//                }
+//            }
+//
+//            if (!exists) {
+//                redCoinCol.add("2");
+//                if (turn % 2 != 0) {
+//                    board[7][2] = "Y";
+//                } else {
+//                    board[7][2] = "R";
+//                }
+//                turn++;
+//            }
+//        } else if (keyCode == KeyEvent.VK_3) {
+//            boolean exists = false;
+//            for (String col : redCoinCol) {
+//                if (col.equals("3")) {
+//                    exists = true;
+//                    break;
+//                }
+//            }
+//
+//            if (!exists) {
+//                redCoinCol.add("3");
+//                if (turn % 2 != 0) {
+//                    board[7][3] = "Y";
+//                } else {
+//                    board[7][3] = "R";
+//                }
+//                turn++;
+//            }
+//        } else if (keyCode == KeyEvent.VK_4) {
+//            boolean exists = false;
+//            for (String col : redCoinCol) {
+//                if (col.equals("4")) {
+//                    exists = true;
+//                    break;
+//                }
+//            }
+//
+//            if (!exists) {
+//                redCoinCol.add("4");
+//                if (turn % 2 != 0) {
+//                    board[7][4] = "Y";
+//                } else {
+//                    board[7][4] = "R";
+//                }
+//                turn++;
+//            }
+//        } else if (keyCode == KeyEvent.VK_5) {
+//            boolean exists = false;
+//            for (String col : redCoinCol) {
+//                if (col.equals("5")) {
+//                    exists = true;
+//                    break;
+//                }
+//            }
+//
+//            if (!exists) {
+//                redCoinCol.add("5");
+//                if (turn % 2 != 0) {
+//                    board[7][5] = "Y";
+//                } else {
+//                    board[7][5] = "R";
+//                }
+//                turn++;
+//            }
+//        } else if (keyCode == KeyEvent.VK_6) {
+//            boolean exists = false;
+//            for (String col : redCoinCol) {
+//                if (col.equals("6")) {
+//                    exists = true;
+//                    break;
+//                }
+//            }
+//
+//            if (!exists) {
+//                redCoinCol.add("6");
+//                if (turn % 2 != 0) {
+//                    board[7][6] = "Y";
+//                } else {
+//                    board[7][6] = "R";
+//                }
+//                turn++;
+//            }
+//        } else if (keyCode == KeyEvent.VK_7) {
+//            boolean exists = false;
+//            for (String col : redCoinCol) {
+//                if (col.equals("7")) {
+//                    exists = true;
+//                    break;
+//                }
+//            }
+//
+//            if (!exists) {
+//                redCoinCol.add("7");
+//                if (turn % 2 != 0) {
+//                    board[7][7] = "Y";
+//                } else {
+//                    board[7][7] = "R";
+//                }
+//                turn++;
+//            }
+//        }
+//
+//        System.out.println(this);
 	}
 
 	@Override
